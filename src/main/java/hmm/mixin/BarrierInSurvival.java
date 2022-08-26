f8f602e1cb524bcf046b1fa23bff51f48165c517
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2022-08-26 15:41:15 +0100

    Added ability to see light and barrier blocks, bumped to 1.5.1

---
+++ src/main/java/hmm/mixin/BarrierInSurvival.java
@@ -0,0 +1,57 @@
+package hmm.mixin;
+
+import java.util.Set;
+import java.util.function.Supplier;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.GameMode;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+
+
+@Mixin(ClientWorld.class)
+public abstract class BarrierInSurvival extends World
+{
+    protected BarrierInSurvival(MutableWorldProperties properties, RegistryKey<World> registryRef,
+            RegistryEntry<DimensionType> dimension, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld,
+            long seed, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, dimension, profiler, isClient, debugWorld, seed, maxChainedNeighborUpdates); }
+
+    @Shadow
+    private MinecraftClient client;
+
+    @Shadow
+    private static Set<Item> BLOCK_MARKER_ITEMS;
+    
+    @Inject(
+        method = "getBlockParticle",
+        at = @At("HEAD"),
+        cancellable = true)
+    private void allowAnyGameMode(CallbackInfoReturnable<Block> ci)
+    {
+        if (this.client.interactionManager.getCurrentGameMode() == GameMode.SURVIVAL)
+        {
+            // Slightly modified copy-paste from target method
+            Item item;
+            
+            if (BLOCK_MARKER_ITEMS.contains(item = (this.client.player.getMainHandStack()).getItem()) && item instanceof BlockItem)
+            {
+                ci.setReturnValue( ((BlockItem)item).getBlock() );
+            }
+        }
+    }
+}
