1ce42de1a4dc9e80150307a64edf19f0e807d8b9
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2022-08-27 13:06:51 +0100

    Allowed use of the select character

---
+++ src/main/java/hmm/mixin/AllowSelectCharacter.java
@@ -0,0 +1,23 @@
+package hmm.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.SharedConstants;
+
+
+@Mixin(SharedConstants.class)
+public abstract class AllowSelectCharacter
+{
+
+    @Inject(
+        method = "isValidChar",
+        at = @At("HEAD"),
+        cancellable = true)
+    private static void replaceSelectCharacter(char chr, CallbackInfoReturnable<Boolean> ci)
+    {
+        if (chr == '\u00a7') ci.setReturnValue(true);
+    }
+}
